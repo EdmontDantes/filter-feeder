a6640334e2ab031688e832cab1f75e41099a0289
Bogdan Kowaltchook <bogdan.kowaltchook@codeimmersives.com>
2020-03-25 12:50:52 -0400

    20 passed, 20 total

--- main.js
+++ main.js
@@ -1,8 +1,78 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const isOdd = function(num) {
+  if (num % 2 === 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const onlyOdds = function(nums) {
+  return nums.filter(isOdd);
+}
+
+const isEven = function(num) {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const onlyEvens = function(nums) {
+  return nums.filter(isEven);
+}
+
+const isShort = function(sentence) {
+  if (sentence.length < 7) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const shortNamesOnly = function(sentences) {
+  return sentences.filter(isShort);
+}
 
 
+const isDName = function(sentence) {
+  if (sentence[0] === 'D' || sentence[0] === 'd') {
+    return true;
+  } else  {
+    return false;
+  }
+}
+
+const dNames = function(sentences) {
+  return sentences.filter(isDName);
+}
+
+const containsLowercase = function(sentence) {
+  if (sentence.toUpperCase() != sentence) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const removeTrolls = function(posts) {
+  return posts.filter(containsLowercase);
+} 
+
+const isTriStateArea = function(location) {
+  if (location.endsWith('NY') || location.endsWith('NJ') || location.endsWith('CT')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const triStateAreaOnly = function(locations) {
+  return locations.filter(isTriStateArea);
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
